f71d2c46b46fb4179f5a3e5d2d52e9558f05a9c0
sunwenjiu <swj33442swj>
2019-08-13 17:15:32 +0800

    小技巧

--- 迭代/小技巧.js
+++ 迭代/小技巧.js
@@ -1,5 +1,5 @@
 java
-
+=======================================================
   public  List<Dict> findAll() {
         Sort sort = Sort.by(Sort.Direction.DESC, "createTime");
         List<Dict> dicts = dictRepository.findAll(sort);
@@ -21,9 +21,68 @@ java
 
         return sortDicts;
     }
-
+	===========================================
+vue
 树节点 隐藏子节点的添加
   style: {
               marginRight: '8px',
               display: (data.parentId === '0')?"inline-block":"none"
             },
+			
+			======================================================================
+
+java
+    /**
+     * 通过子节点类型名字，找出一级分类
+     *
+     * @param typeName
+     * @return
+     */
+    private String getEventParentByEventTypeName(String typeName) {
+
+        List<EventType> eventTypes = eventTypeRepository.findAll();
+        String typeId = null;
+        for (EventType e : eventTypes
+                ) {
+            if (e.getName().equals(typeName)) {
+                typeId = e.getEventTypeId();
+            }
+        }
+        if (typeId == null) {
+            throw new ParameterInvalidException("传入的事件类型名字有误");
+        }
+        String id = getEventParentName(eventTypes, typeId);
+
+        return id;
+    }
+
+    /**
+     * 通过子节点类型名字，找出一级分类的递归
+     *
+     * @param list
+     * @param typeID
+     * @return
+     */
+    private String getEventParentName(List<EventType> list, String typeID) {
+        String eventId = null;
+
+        String parent = null;
+        for (EventType e : list
+                ) {
+            if (e.getEventTypeId().equals(typeID)) {
+                parent = e.getParentId();
+                if (!parent.equals("0")) {
+                    eventId= getEventParentName(list, parent);
+                } else {
+                    eventId = e.getEventTypeId();
+
+                    break;
+                }
+
+            }
+        }
+
+        return eventId;
+    }
+	
+	========================================================================
